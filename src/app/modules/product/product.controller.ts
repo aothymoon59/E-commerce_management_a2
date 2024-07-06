5d030705be03237747f45af9838ba88e32f5976b
aothymoon59 <aothymoon59@gmail.com>
2024-07-06 22:25:33 +0600

    [ADD] getAllProducts in controller

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -24,6 +24,27 @@ const createProduct = async (req: Request, res: Response) => {
   }
 };
 
+// Retrieve a list of all users
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProductsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Products fetched successfully!',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message || 'Something went wrong',
+      error: error,
+    });
+  }
+};
+
 export const ProductControllers = {
   createProduct,
+  getAllProducts,
 };
