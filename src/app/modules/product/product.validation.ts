7851b03384aca77020997179ab44c859321bc727
aothymoon59 <aothymoon59@gmail.com>
2024-07-06 20:26:47 +0600

    [ADD] created produc validation schema using zod

---
+++ src/app/modules/product/product.validation.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+const createProductValidationSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  tags: z.array(z.string()),
+  variants: z.array(
+    z.object({
+      type: z.string(),
+      value: z.string(),
+    }),
+  ),
+  inventory: z.object({
+    quantity: z.number(),
+    inStock: z.boolean(),
+  }),
+});
+
+export default createProductValidationSchema;
