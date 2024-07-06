4375a578e55b7ff64a40a56c04a914204d5ff8e4
aothymoon59 <aothymoon59@gmail.com>
2024-07-06 20:03:31 +0600

    [ADD] created product interface

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import config from './app/config';
 // import { StudentRoutes } from "./app/modules/student/student.route";
 const app: Application = express();
 
@@ -10,8 +11,8 @@ app.use(cors());
 // application routes
 // app.use("/api/v1/students", StudentRoutes);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send(`E-commerce Management Server is running on port ${config.port}!`);
 });
 
 export default app;
