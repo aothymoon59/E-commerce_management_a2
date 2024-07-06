cf60d5bb427df5d65d4465e821e371edeb1daa83
aothymoon59 <aothymoon59@gmail.com>
2024-07-06 20:41:25 +0600

    [ADD] crerateProduct in controllers

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,12 +1,12 @@
 import { TProduct } from './product.interface';
 import { Product } from './product.model';
 
-// Create a new product
-const createNewProductIntoDB = async (productData: TProduct) => {
+// Create a product
+const createProductIntoDB = async (productData: TProduct) => {
   const result = await Product.create(productData);
   return result;
 };
 
-export const productServices = {
-  createNewProductIntoDB,
+export const ProductServices = {
+  createProductIntoDB,
 };
