f7dc98429e0633d46f0544f6312cbd68b2267c44
aothymoon59 <aothymoon59@gmail.com>
2024-07-07 13:58:44 +0600

    [ADD] order model created

---
+++ src/app/modules/orders/order.model.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from 'mongoose';
+import { TOrder } from './order.interface';
+
+const OrderSchema = new Schema<TOrder>({
+  email: {
+    type: String,
+  },
+  productId: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  quantity: {
+    type: Number,
+  },
+});
+export const Order = model<TOrder>('Order', OrderSchema);
